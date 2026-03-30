12fec99a7690c27c64dd0d5acdca7594ed2ad549
Ismail Abokhalifa <Ismail.Abokhalifa2002@gmail.com>
2026-03-30 06:35:20 +0200

    gave a simple example for useEffect hook as when you render the page, it works once at least

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,18 +1,32 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Counter = ()=> {
     const [num, setNum] = useState<number>(0);
+    const [success, setSuccess] = useState<boolean>(false);
 
-    const increament = ()=> {
+    const increment = ()=> {
         setNum(num + 1);
-        console.log("Triggered rerender");
+
+        if(num === 10){
+            setSuccess(true);
+        }
     }
+    useEffect(()=> {
+        console.log("# Event is success");
+        
+    }, [success])
+    
+    useEffect(()=> {
+        console.log("## Num is triggered");
+        
+    }, [num])
+
     return (
         <main className="my-5">
           <h1 className="text-2xl text-gray-300">Counter Component</h1>
           <div>Num: {num}</div>
-          <button className="btn cursor-pointer" onClick={increament}>Click to Increament</button>
+          <button className="btn cursor-pointer" onClick={increment}>Click to Increment</button>
         </main>
     )
 }
