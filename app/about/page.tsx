a4158d601aba97d37f14a636e5df1b2bbdc67075
Ismail Abokhalifa <Ismail.Abokhalifa2002@gmail.com>
2026-04-01 11:55:32 +0200

    created a window innerWidth's resize setter function

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,34 @@
+"use client";
 import Link from "next/link";
 import Header from "@/components/Header";
 import Description from "@/components/Description";
+import React, { useState, useEffect } from "react";
+
+
+
+
+
 const About = ()=> {
+    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+    const handleResize = ()=> {
+    setWindowWidth(window.innerWidth)
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+    },[])
+
     return (
         <div className="w-screen h-screen bg-gray-900">
             <h1 className="text-3xl text-teal-200">About Page</h1>
             <Header text="Header Component" />
             <Description />
+            <div className="m-10 text-white">{windowWidth}</div>
             <br />
             <Link href="/" className="text-blue-500 text-xl">Go To Home</Link>
+            
+
         </div>
     )
 }
